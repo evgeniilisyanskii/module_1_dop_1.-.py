0a23b1ad9d9af04bad52806884fab2a066755480
evgeniilisyanskii <evgeniilisyanskii@yandex.ru>
2024-08-23 00:56:09 +1000

    Update module_1_dop_1.ру.py

--- module_1_dop_1.ру.py
+++ module_1_dop_1.ру.py
@@ -3,5 +3,5 @@ students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 a = (sum(grades[0])/len(grades[0]), sum(grades[1])/len(grades[1]), sum(grades[2])/len(grades[2]),
      sum(grades[3])/len(grades[3]), sum(grades[4])/len(grades[4]))
 students_sort = sorted(students)
-klass = dict(zip(students_sort,a)
+klass = dict(zip(students_sort,a))
 print(klass)
